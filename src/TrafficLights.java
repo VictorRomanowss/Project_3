c0c6e89e05a4560b6770be653f934334ab2da37d
Виктор Романов <romanow.vitya2018@icloud.com>
2019-03-02 00:30:49 +0300

    init project

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -8,35 +8,38 @@ public class TrafficLights {
 
         int color;
         Scanner scr = new Scanner(System.in);
-        while(true){
-        System.out.println("Введите число которе будет отвечать за цвет: 1-краcный, 2-желтый, 3-зеленый,4-мигающий желтый");
+        while (true) {
+            System.out.println("Введите число которе будет отвечать за цвет: 1-краcный, 2-желтый, 3-зеленый,4-мигающий желтый");
 //        Scanner scr = new Scanner(System.in);
-        color = scr.nextInt();
-        int time = 5;
+            color = scr.nextInt();
+            int time = 5;
 
-        if (color == 1) {
-            System.out.println("Загорается красный");
-            do {
-                System.out.println("Время работы светофора " + time + " секунд");
+            if (color == 1) {
+                System.out.println("Загорается красный");
+                do {
+                    System.out.println("Время работы светофора " + time + " секунд");
+                }
+                while (--time > 0);
+
+            } else if (color == 2) {
+                do {
+                    System.out.println("Время работы светофора " + time + " секунд");
+                }
+                while (--time > 0);
+
+                System.out.println("Загорается желтый");
+            } else if (color == 3) {
+                do {
+                    System.out.println("Время работы светофора " + time + " секунд");
+                }
+                while (--time > 0);
+
+                System.out.println("Загорается зеленый");
+            } else if (color == 4) {
+
+                System.out.println("Мигающий желтый");
             }
-            while (--time > 0);
-
-        } else if (color == 2) {
-            do {
-                System.out.println("Время работы светофора " + time + " секунд");
-            }
-            while (--time > 0);
-
-            System.out.println("Загорается желтый");
-        } else if (color == 3) {
-            do {
-                System.out.println("Время работы светофора " + time + " секунд");
-            }
-            while (--time > 0);
-
-            System.out.println("Загорается зеленый");
-        } else if (color == 4) {
-
-            System.out.println("Мигающий желтый");
-        }}}}
+        }
+    }
+}
 
